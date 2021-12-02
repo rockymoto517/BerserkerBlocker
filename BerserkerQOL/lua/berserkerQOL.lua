4346f8283a864bb99e0a555c951ea7a160365768
rockymoto517 <74614475+rockymoto517@users.noreply.github.com>
2021-12-01 22:30:49 -0600

    Remake Tag Team code

--- BerserkerQOL/lua/berserkerQOL.lua
+++ BerserkerQOL/lua/berserkerQOL.lua
@@ -76,11 +76,55 @@ end
 
 if RequiredScript == "lib/player_actions/skills/playeractiontagteam" then
     --Disable Tag Team Vape
-    local old_tag_team_tagged = PlayerAction.TagTeamTagged.Function
-    PlayerAction.TagTeamTagged.Function = function (...)
-        if berserkerQOL._data["tt_cancer"] == 2 and berserkerQOL._has_zerk or berserkerQOL._data["tt_cancer"] == 3 then
+    PlayerAction.TagTeamTagged.Function = function (tagged, owner)
+		if tagged ~= managers.player:local_player() then
 			return
 		end
-		old_tag_team_tagged(...)
-    end
+
+		local base_values = owner:base():upgrade_value("player", "tag_team_base")
+		local kill_health_gain = base_values.kill_health_gain * base_values.tagged_health_gain_ratio
+		local timer = TimerManager:game()
+		local end_time = timer:time() + base_values.duration
+		local on_damage_key = {}
+
+		local function on_damage(damage_info)
+			local was_killed = damage_info.result.type == "death"
+			local valid_player = damage_info.attacker_unit == owner or damage_info.attacker_unit == tagged
+			if was_killed and valid_player then
+				end_time = math.min(end_time + base_values.kill_extension, timer:time() + base_values.duration)
+
+				tagged:character_damage():restore_health(kill_health_gain, true)
+			end
+		end
+
+		if berserkerQOL._data["tt_cancer"] == 2 and berserkerQOL._has_zerk or berserkerQOL._data["tt_cancer"] == 3 then 
+			return
+		else
+			CopDamage.register_listener(on_damage_key, {
+				"on_damage"
+			}, on_damage)
+		end
+
+		local ended_by_owner = false
+		local on_end_key = {}
+
+		local function on_action_end(end_tagged, end_owner)
+			local tagged_match = tagged == end_tagged
+			local owner_match = owner == end_owner
+			ended_by_owner = tagged_match and owner_match
+		end
+
+		managers.player:add_listener(on_end_key, {
+			"tag_team_end"
+		}, on_action_end)
+
+		local timer = TimerManager:game()
+
+		while not ended_by_owner and alive(tagged) and (alive(owner) or timer:time() < end_time) do
+			coroutine.yield()
+		end
+
+		CopDamage.unregister_listener(on_damage_key)
+		managers.player:remove_listener(on_end_key)
+	end
 end
