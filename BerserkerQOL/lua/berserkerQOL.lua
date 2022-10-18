6085c922ee37db975458658e40efcb85520f5014
Rockymoto517 <Rockymoto517@gmail.com>
2022-10-18 16:07:15 -0500

    Multiple Profile Support + Remove Hacker/TagTeam Cancer

--- BerserkerQOL/lua/berserkerQOL.lua
+++ BerserkerQOL/lua/berserkerQOL.lua
@@ -1,168 +1,97 @@
 dofile(ModPath .. "lua/init.lua")
---Initialize Table
+-- require("lib/managers/multiprofilemanager")
+-- Initialize Table
 
 --[[
-	1: Never block (never used directly)
-	2: If you have berserker
-	3: Always block
+	1: Inherit from default
+	2: Never block (never used directly)
+	3: If you have berserker
+	4: Always block
+	5: Block if you have quick fix
 ]]
 
-local function __check_table(index)
-	if index then
-		return index == 2 and berserkerQOL._has_zerk or index == 3
+-- return the blocking status
+local function __check_table(effect)
+    _profile = managers.multi_profile and managers.multi_profile._global._current_profile or 31
+    if berserkerQOL._data['skillset'..tostring(_profile)][effect] ~= 1 and berserkerQOL._data['skillset'..tostring(_profile)][effect] ~= 0 then
+        if effect == "qf_cancer" then
+            value = (berserkerQOL._data['skillset'..tostring(_profile)][effect] == 3 and berserkerQOL._has_zerk) or
+					(berserkerQOL._data['skillset'..tostring(_profile)][effect] == 4) or
+					(berserkerQOL._data['skillset'..tostring(_profile)][effect] == 5 and berserkerQOL._has_qfaced == false)
+            berserkerQOL:log(effect, tostring(value))
+			return value
+		end
+
+        value = (berserkerQOL._data['skillset'..tostring(_profile)][effect] == 3 and berserkerQOL._has_zerk) or
+				(berserkerQOL._data['skillset'..tostring(_profile)][effect] == 4)
+        berserkerQOL:log(effect, tostring(value))
+		return value
+    else
+        if effect == "qf_cancer" then
+            value = (berserkerQOL._data.default[effect] == 3 and berserkerQOL._has_zerk) or
+					(berserkerQOL._data.default[effect] == 4) or
+					(berserkerQOL._data.default[effect] == 5 and berserkerQOL._has_qfaced == false)
+            berserkerQOL:log(effect, tostring(value))
+			return value
+		end
+
+        value = (berserkerQOL._data.default[effect] == 3 and berserkerQOL._has_zerk) or
+				(berserkerQOL._data.default[effect] == 4)
+        berserkerQOL:log(effect, tostring(value))
+		return value
 	end
 end
 
 if RequiredScript == "lib/units/beings/player/playerinventory" then
-    --Disable Hacker ECM
-    Hooks:PostHook(PlayerInventory,"_start_feedback_effect", "Berserker_qol_start_feedback_effect", 
-    function(self, ...)
-    	local is_player = managers.player:player_unit() == self._unit
-    	if not is_player and self._jammer_data and self._jammer_data.heal then
-			if __check_table(berserkerQOL._data["hacker_cancer"]) then
-    			self._jammer_data.heal = 0
-			end
-    	end
-    end 
-    )
-	
-	--Check if the player has berserker
-	Hooks:PostHook(PlayerInventory,"init", "Berserker_qol_find_zerk", 
-    function(self, ...)
+	-- Check if the player has berserker
+	Hooks:PostHook(PlayerInventory,"init", "Berserker_qol_find_zerk",
+    	function(self, ...)
 		if managers.player:has_category_upgrade("player", "damage_health_ratio_multiplier") or managers.player:has_category_upgrade("player", "melee_damage_health_ratio_multiplier") then
 			berserkerQOL._has_zerk = true
 		end
 		if managers.player:has_category_upgrade("temporary", "temporary_first_aid_damage_reduction") then
 			berserkerQOL._has_qfaced = true
-		else 
+		else
 			berserkerQOL._has_qfaced = false
 		end
-	end
-	)
+	end)
 end
 
 if RequiredScript == "lib/units/beings/player/playerdamage" then
+    -- Check for combat medic
 	function PlayerDamage:set_revive_boost(revive_health_level)
-		if __check_table(berserkerQOL._data["combatmedic_cancer"]) then self._revive_health_multiplier = 1 else
+		if __check_table("combatmedic_cancer") then self._revive_health_multiplier = 1 else
 		self._revive_health_multiplier = tweak_data.upgrades.revive_health_multiplier[revive_health_level] end
 	end
 end
 
 if RequiredScript == "lib/network/handlers/unitnetworkhandler" then
-	function UnitNetworkHandler:sync_cocaine_stacks(amount, in_use, upgrade_level, power_level, sender)
-		local peer = self._verify_sender(sender)
-	
-		if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not peer then
-			return
-		end
-	
-		local peer_id = peer:id()
-		local current_cocaine_stacks = managers.player:get_synced_cocaine_stacks(peer_id)
-		local bqolmod_bool = __check_table(berserkerQOL._data["maniac_cancer"])
-		
-		if current_cocaine_stacks and not bqolmod_bool then
-			amount = math.min((current_cocaine_stacks and current_cocaine_stacks.amount or 0) + (tweak_data.upgrades.max_cocaine_stacks_per_tick or 20), amount)
-		end
-
-		if bqolmod_bool then
-			amount = 0
-		end
-	
-		managers.player:set_synced_cocaine_stacks(peer_id, amount, in_use, upgrade_level, power_level)
-	end
+    -- Set maniac stacks to 0
+    Hooks:PostHook(UnitNetworkHandler, "sync_cocaine_stacks", "berserkerQOL_sync_cocaine_stacks",
+    function(self, amount, ...)
+        if __check_table("maniac_cancer") then
+            amount = 0
+        end
+    end)
 
 	--If the upgrades are CC TEAM upgrades, don't add them to the player
 	function UnitNetworkHandler:add_synced_team_upgrade(category, upgrade, level, sender)
 		local sender_peer = self._verify_sender(sender)
-	
+
 		if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not sender_peer then
 			return
 		end
-	
+
 		local peer_id = sender_peer:id()
 
-		if __check_table(berserkerQOL._data["cc_cancer"]) then --check if they want it disabled on zerk and have zerk or want it always disabled (this gets used a lot)
+		if __check_table("cc_cancer") then --check if they want it disabled on zerk and have zerk or want it always disabled (this gets used a lot)
 			if category ~= "health" and category ~= "armor" and category ~= "damage_dampener" and category ~= "hostage_multiplier" and category ~= "passive_hostage_multiplier" then
 				managers.player:add_synced_team_upgrade(peer_id, category, upgrade, level)
 			end
 		else
 			managers.player:add_synced_team_upgrade(peer_id, category, upgrade, level)
 		end
-		
-	end
 
-	function UnitNetworkHandler:copr_teammate_heal(healer_unit, upgrade_level, sender)
-		if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_character_and_sender(healer_unit, sender) then
-			return
-		end
-	
-		local player_unit = managers.player:player_unit()
-		local character_damage = alive(player_unit) and player_unit:character_damage()
-	
-		if not character_damage or not character_damage.on_copr_heal_received then
-			return
-		end
-		
-		if __check_table(berserkerQOL._data["leech_cancer"]) then
-		else
-			character_damage:on_copr_heal_received(healer_unit, upgrade_level)
-		end
-	end
-
-end
-
-if RequiredScript == "lib/player_actions/skills/playeractiontagteam" then
-    --Disable Tag Team Vape
-    PlayerAction.TagTeamTagged.Function = function (tagged, owner)
-		if tagged ~= managers.player:local_player() then
-			return
-		end
-
-		local base_values = owner:base():upgrade_value("player", "tag_team_base")
-		local kill_health_gain = base_values.kill_health_gain * base_values.tagged_health_gain_ratio
-		local timer = TimerManager:game()
-		local end_time = timer:time() + base_values.duration
-		local on_damage_key = {}
-
-		local function on_damage(damage_info)
-			local was_killed = damage_info.result.type == "death"
-			local valid_player = damage_info.attacker_unit == owner or damage_info.attacker_unit == tagged
-			if was_killed and valid_player then
-				end_time = math.min(end_time + base_values.kill_extension, timer:time() + base_values.duration)
-
-				tagged:character_damage():restore_health(kill_health_gain, true)
-			end
-		end
-
-		if __check_table(berserkerQOL._data["tt_cancer"]) then 
-			return
-		else
-			CopDamage.register_listener(on_damage_key, {
-				"on_damage"
-			}, on_damage)
-		end
-
-		local ended_by_owner = false
-		local on_end_key = {}
-
-		local function on_action_end(end_tagged, end_owner)
-			local tagged_match = tagged == end_tagged
-			local owner_match = owner == end_owner
-			ended_by_owner = tagged_match and owner_match
-		end
-
-		managers.player:add_listener(on_end_key, {
-			"tag_team_end"
-		}, on_action_end)
-
-		local timer = TimerManager:game()
-
-		while not ended_by_owner and alive(tagged) and (alive(owner) or timer:time() < end_time) do
-			coroutine.yield()
-		end
-
-		CopDamage.unregister_listener(on_damage_key)
-		managers.player:remove_listener(on_end_key)
 	end
 end
 
@@ -175,7 +104,7 @@ if RequiredScript == "lib/managers/playermanager" then
 		multiplier = multiplier * self:temporary_upgrade_value("temporary", "revived_damage_resist", 1)
 		multiplier = multiplier * self:upgrade_value("player", "damage_dampener", 1)
 		multiplier = multiplier * self:upgrade_value("player", "health_damage_reduction", 1)
-		if __check_table(berserkerQOL._data["qf_cancer"]) or (berserkerQOL._data == 4 and berserkerQOL._has_qfaced == false) then
+		if __check_table("qf_cancer") then
 		else
 			multiplier = multiplier * self:temporary_upgrade_value("temporary", "first_aid_damage_reduction", 1)
 		end
@@ -184,70 +113,62 @@ if RequiredScript == "lib/managers/playermanager" then
 		multiplier = multiplier * self._properties:get_property("revive_damage_reduction", 1)
 		multiplier = multiplier * self._temporary_properties:get_property("revived_damage_reduction", 1)
 		local dmg_red_mul = self:team_upgrade_value("damage_dampener", "team_damage_reduction", 1)
-	
+
 		if self:has_category_upgrade("player", "passive_damage_reduction") then
 			local health_ratio = self:player_unit():character_damage():health_ratio()
 			local min_ratio = self:upgrade_value("player", "passive_damage_reduction")
-	
+
 			if health_ratio < min_ratio then
 				dmg_red_mul = dmg_red_mul - (1 - dmg_red_mul)
 			end
 		end
-	
+
 		multiplier = multiplier * dmg_red_mul
-	
+
 		if damage_type == "melee" then
 			multiplier = multiplier * managers.player:upgrade_value("player", "melee_damage_dampener", 1)
 		end
-	
+
 		local current_state = self:get_current_state()
-	
+
 		if current_state and current_state:_interacting() then
 			multiplier = multiplier * managers.player:upgrade_value("player", "interacting_damage_multiplier", 1)
 		end
-	
+
 		return multiplier
 	end
 
 	function PlayerManager:body_armor_skill_addend(override_armor)
 		local addend = 0
 		addend = addend + self:upgrade_value("player", tostring(override_armor or managers.blackmarket:equipped_armor(true, true)) .. "_armor_addend", 0)
-	
+
 		if self:has_category_upgrade("player", "armor_increase") then
 			local health_multiplier = self:health_skill_multiplier()
 			local max_health = (PlayerDamage._HEALTH_INIT + self:health_skill_addend()) * health_multiplier
 			addend = addend + max_health * self:upgrade_value("player", "armor_increase", 1)
 		end
-		
-		if __check_table(berserkerQOL._data["ai_armor_cancer"]) then return addend else
 
-		addend = addend + self:upgrade_value("team", "crew_add_armor", 0) end
-	
-		return addend
-	end
-
-	function PlayerManager:fixed_health_regen(health_ratio)
-		local health_regen = 0
-	
-		if not health_ratio or not self:is_damage_health_ratio_active(health_ratio) then
-			if __check_table(berserkerQOL._data["ai_hp_cancer"]) then return health_regen else
-
-			health_regen = health_regen + self:upgrade_value("team", "crew_health_regen", 0) end
+		if __check_table("ai_armor_cancer") then return
+			addend
 		end
-	
-		return health_regen
+
+		addend = addend + self:upgrade_value("team", "crew_add_armor", 0)
+
+		return addend
 	end
 
 	function PlayerManager:health_skill_addend()
 		local addend = 0
-		if __check_table(berserkerQOL._data["ai_hp_cancer"]) then return addend	else 
-		
-		addend = addend + self:upgrade_value("team", "crew_add_health", 0) end
-	
+		if __check_table("ai_hp_cancer") then
+			return addend
+		end
+
+		addend = addend + self:upgrade_value("team", "crew_add_health", 0)
+
 		if table.contains(self._global.kit.equipment_slots, "thick_skin") then
 			addend = addend + self:upgrade_value("player", "thick_skin", 0)
 		end
-	
+
 		return addend
 	end
 end
--- BerserkerQOL/lua/init.lua
+++ BerserkerQOL/lua/init.lua
@@ -3,17 +3,28 @@ if not _G.berserkerQOL then
 	berserkerQOL._path = ModPath
 	berserkerQOL._data_path = SavePath .. "berserkerQOL_save_data.txt"
 	berserkerQOL._data = {}
-	berserkerQOL._data["cc_cancer"] = 0
-	berserkerQOL._data["maniac_cancer"] = 0
-	berserkerQOL._data["hacker_cancer"] = 0
-	berserkerQOL._data["tt_cancer"] = 0
-	berserkerQOL._data["leech_cancer"] = 0
-	berserkerQOL._data["qf_cancer"] = 0
-	berserkerQOL._data["ai_armor_cancer"] = 0
-	berserkerQOL._data["ai_hp_cancer"] = 0
-	berserkerQOL._data["combatmedic_cancer"] = 0
+    berserkerQOL._data.default = {}
+	berserkerQOL._data.default["cc_cancer"] = 0
+	berserkerQOL._data.default["maniac_cancer"] = 0
+	berserkerQOL._data.default["qf_cancer"] = 0
+	berserkerQOL._data.default["ai_armor_cancer"] = 0
+	berserkerQOL._data.default["ai_hp_cancer"] = 0
+	berserkerQOL._data.default["combatmedic_cancer"] = 0
 	berserkerQOL._has_zerk = false
 	berserkerQOL._has_qfaced = false
+	local NUM_PROFILES = #tweak_data.skilltree.skill_switches or 15
+
+	for i = 1, NUM_PROFILES do
+		berserkerQOL._data['skillset'..tostring(i)] = {
+				["cc_cancer"] = 0,
+				["maniac_cancer"] = 0,
+				["qf_cancer"] = 0,
+				["ai_armor_cancer"] = 0,
+				["ai_hp_cancer"] = 0,
+				["combatmedic_cancer"] = 0
+		}
+	end
+	debug_on = false
 
 	--Build the menu
 	function berserkerQOL:Save()
@@ -34,6 +45,12 @@ if not _G.berserkerQOL then
 		end
 	end
 
+    function berserkerQOL:log(effect, text)
+		if debug_on and effect ~= "ai_armor_cancer" and effect ~= "ai_hp_cancer" then
+        	log(string.format("[BQOL Debug] %s = %s", effect, text))
+		end
+    end
+
 	local save_exists = io.open(berserkerQOL._data_path, "r")
 	if save_exists ~= nil then
 		save_exists:close()
@@ -42,69 +59,161 @@ if not _G.berserkerQOL then
 		berserkerQOL:Save()
 	end
 
-	Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInit_berserkerQOL", function( loc )
-		loc:load_localization_file( berserkerQOL._path .. "menu/en.txt")
+    -- Helper function
+    local function refresh_options()
+        local _bqol_menu = MenuHelper:GetMenu("berserkerQOL_menu") or {_items = {}}
+        for _, item in pairs(_bqol_menu._items) do
+            if item._parameters and item._parameters.name == "berserkerQOL_skillset" then
+                if item._options then
+                    for i = 1, NUM_PROFILES do
+                        local profile = managers.multi_profile._global._profiles[i]
+                        item._options[i+1]._parameters.text_id = profile and profile.name or "Profile "..tostring(i)
+                    end
+                end
+                break
+            end
+        end
+    end
+
+    -- Returns the updated value of the menu item
+	local function update_value(index, profile)
+		local ret = (profile == 31 and berserkerQOL._data.default[index]) or (berserkerQOL._data['skillset'..tostring(profile)][index])
+		return ret ~= 0 and ret or 1
+	end
+
+	Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInit_berserkerQOL", function(loc)
+		loc:load_localization_file(berserkerQOL._path .. "menu/en.txt")
 	end)
 
-	Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_berserkerQOL", function( menu_manager )
+	Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_berserkerQOL", function(menu_manager)
+		local selectedProfile = NUM_PROFILES + 1
 
 		--Gather the options
 		MenuCallbackHandler.berserkerQOL_callback_cc = function(self, item)
-			berserkerQOL._data.berserkerQOL_cc_value = item:value()
-			berserkerQOL._data["cc_cancer"] = berserkerQOL._data.berserkerQOL_cc_value
-			berserkerQOL:Save()
+			if selectedProfile == NUM_PROFILES + 1 then
+				berserkerQOL._data.default["cc_cancer"] = item:value()
+				berserkerQOL._data._cc_cancer = item:value()
+			else
+				berserkerQOL._data['skillset'..tostring(selectedProfile)]["cc_cancer"] = item:value()
+			end
 		end
 
 		MenuCallbackHandler.berserkerQOL_callback_maniac = function(self, item)
-			berserkerQOL._data.berserkerQOL_maniac_value = item:value()
-			berserkerQOL._data["maniac_cancer"] = berserkerQOL._data.berserkerQOL_maniac_value
-			berserkerQOL:Save()
-		end
-
-		MenuCallbackHandler.berserkerQOL_callback_hacker = function(self, item)
-			berserkerQOL._data.berserkerQOL_hacker_value = item:value()
-			berserkerQOL._data["hacker_cancer"] = berserkerQOL._data.berserkerQOL_hacker_value
-			berserkerQOL:Save()
-		end
-
-		MenuCallbackHandler.berserkerQOL_callback_tt = function(self, item)
-			berserkerQOL._data.berserkerQOL_tt_value = item:value()
-			berserkerQOL._data["tt_cancer"] = berserkerQOL._data.berserkerQOL_tt_value
-			berserkerQOL:Save()
+			if selectedProfile == NUM_PROFILES + 1 then
+				berserkerQOL._data.default["maniac_cancer"] = item:value()
+				berserkerQOL._data._maniac_cancer = item:value()
+			else
+				berserkerQOL._data['skillset'..tostring(selectedProfile)]["maniac_cancer"] = item:value()
+			end
 		end
 
 		MenuCallbackHandler.berserkerQOL_callback_qf = function(self, item)
-			berserkerQOL._data.berserkerQOL_qf_value = item:value()
-			berserkerQOL._data["qf_cancer"] = berserkerQOL._data.berserkerQOL_qf_value
-			berserkerQOL:Save()
-		end
-
-		MenuCallbackHandler.berserkerQOL_callback_leech = function(self, item)
-			berserkerQOL._data.berserkerQOL_leech_value = item:value()
-			berserkerQOL._data["leech_cancer"] = berserkerQOL._data.berserkerQOL_leech_value
-			berserkerQOL:Save()
+			if selectedProfile == NUM_PROFILES + 1 then
+				berserkerQOL._data.default["qf_cancer"] = item:value()
+				berserkerQOL._data._qf_cancer = item:value()
+			else
+				berserkerQOL._data['skillset'..tostring(selectedProfile)]["qf_cancer"] = item:value()
+			end
 		end
 
 		MenuCallbackHandler.berserkerQOL_callback_ai_armor = function(self, item)
-			berserkerQOL._data.berserkerQOL_ai_armor_value = item:value()
-			berserkerQOL._data["ai_armor_cancer"] = berserkerQOL._data.berserkerQOL_ai_armor_value
-			berserkerQOL:Save()
+			if selectedProfile == NUM_PROFILES + 1 then
+				berserkerQOL._data.default["ai_armor_cancer"] = item:value()
+				berserkerQOL._data._ai_armor_cancer = item:value()
+			else
+				berserkerQOL._data['skillset'..tostring(selectedProfile)]["ai_armor_cancer"] = item:value()
+			end
 		end
 
 		MenuCallbackHandler.berserkerQOL_callback_ai_hp = function(self, item)
-			berserkerQOL._data.berserkerQOL_ai_hp_value = item:value()
-			berserkerQOL._data["ai_hp_cancer"] = berserkerQOL._data.berserkerQOL_ai_hp_value
-			berserkerQOL:Save()
+			if selectedProfile == NUM_PROFILES + 1 then
+				berserkerQOL._data.default["ai_hp_cancer"] = item:value()
+				berserkerQOL._data._ai_hp_cancer = item:value()
+			else
+				berserkerQOL._data['skillset'..tostring(selectedProfile)]["ai_hp_cancer"] = item:value()
+			end
 		end
 
 		MenuCallbackHandler.berserkerQOL_callback_cm = function(self, item)
-			berserkerQOL._data.berserkerQOL_cm_value = item:value()
-			berserkerQOL._data["combatmedic_cancer"] = berserkerQOL._data.berserkerQOL_cm_value
-			berserkerQOL:Save()
+			if selectedProfile == NUM_PROFILES + 1 then
+				berserkerQOL._data.default["combatmedic_cancer"] = item:value()
+				berserkerQOL._data._combatmedic_cancer = item:value()
+			else
+				berserkerQOL._data['skillset'..tostring(selectedProfile)]["combatmedic_cancer"] = item:value()
+			end
 		end
+
+		MenuCallbackHandler.berserkerQOL_callback_skillset = function(self, item)
+
+			selectedProfile = tonumber(item:value())
+
+			local _bqol_menu = MenuHelper:GetMenu("berserkerQOL_menu") or {_items = {}}
+
+            -- Update the displayed options every time we switch skillsets
+            for _, item in pairs(_bqol_menu._items) do
+				local name = item._parameters.name
+
+				if item._type == "multi_choice" and name ~= "berserkerQOL_skillset" then
+					if name == "berserkerQOL_cc" then
+						item:set_current_index(update_value("cc_cancer", selectedProfile))
+					elseif name == "berserkerQOL_maniac" then
+						item:set_current_index(update_value("maniac_cancer", selectedProfile))
+					elseif name == "berserkerQOL_qf" then
+						item:set_current_index(update_value("qf_cancer", selectedProfile))
+					elseif name == "berserkerQOL_ai_armor" then
+						item:set_current_index(update_value("ai_armor_cancer", selectedProfile))
+					elseif name == "berserkerQOL_ai_hp" then
+						item:set_current_index(update_value("ai_hp_cancer", selectedProfile))
+					elseif name == "berserkerQOL_cm" then
+						item:set_current_index(update_value("combatmedic_cancer", selectedProfile))
+					end
+				end
+			end
+
+			_bqol_menu = nil
+		end
+
+		MenuCallbackHandler.berserkerQOL_callback_apply = function(self)
+			berserkerQOL:Save()
+            -- Exit and enter menu on save to make it feel responsive
+			managers.menu:back()
+			managers.menu:open_node('berserkerQOL_menu')
+		end
+
+        -- Add every profile to the profile option
+        local populated_menu
+        MenuCallbackHandler.berserkerQOL_focus_changed_callback = function(self)
+            COUNT_PROFILES = managers.multi_profile and managers.multi_profile:profile_count() or 3
+
+            if populated_menu then
+                refresh_options()
+                return
+            end
+
+            local _bqol_menu = MenuHelper:GetMenu("berserkerQOL_menu") or {_items = {}}
+            for _, item in pairs(_bqol_menu._items) do
+                if item._parameters and item._parameters.name == "berserkerQOL_skillset" then
+					item:add_option(CoreMenuItemOption.ItemOption:new({
+						_meta = "option",
+						text_id = "default",
+						value = COUNT_PROFILES + 1,
+						localize = false
+                    }))
+                    for i = 1, COUNT_PROFILES do
+                        item:add_option(CoreMenuItemOption.ItemOption:new({
+							_meta = "option",
+							text_id = managers.multi_profile and managers.multi_profile._global._profiles[i].name or "Profile "..tostring(i),
+							value = i,
+							localize = false
+                        }))
+                    end
+                    break
+                end
+            end
+            populated_menu = true
+        end
 
 		berserkerQOL:Load()
 		MenuHelper:LoadFromJsonFile(berserkerQOL._path .. "menu/options.json", berserkerQOL, berserkerQOL._data)
-
 	end)
 end
